265f2959862fd84413854ec53d88c4ab6cc65411
chae-ring <149919388+chae-ring@users.noreply.github.com>
2026-05-16 15:11:40 +0900

    init: create spring boot backend project

---
+++ src/main/java/com/tikitaka/backend/global/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.tikitaka.backend.global;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "TikiTaka backend is running";
+    }
+}
