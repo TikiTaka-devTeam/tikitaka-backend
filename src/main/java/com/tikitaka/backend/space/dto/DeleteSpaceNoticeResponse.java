57986ecdaf073c001639c548f3cc319c08fd99ac
chae-ring <149919388+chae-ring@users.noreply.github.com>
2026-07-04 20:35:41 +0900

    fix : 공지 spaceNoticedelete dto 수정

--- src/main/java/com/tikitaka/backend/space/dto/DeleteSpaceNoticeResponse.java
+++ src/main/java/com/tikitaka/backend/space/dto/DeleteSpaceNoticeResponse.java
@@ -1,6 +1,11 @@
 package com.tikitaka.backend.space.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "스페이스 공지 삭제 응답")
 public record DeleteSpaceNoticeResponse(
+
+        @Schema(description = "삭제 결과 메시지", example = "공지가 삭제되었습니다.")
         String message
 ) {
 }
