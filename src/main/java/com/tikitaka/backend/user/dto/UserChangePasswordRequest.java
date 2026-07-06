cb301cf3d18e7c1cf170fb353af9f7df18305fc0
Novice-Dev-Robin <dev.robin03@gmail.com>
2026-07-06 14:59:42 +0900

    비밀번호 변경 시에도, 8자 이상 유지 조건 추가

--- src/main/java/com/tikitaka/backend/user/dto/UserChangePasswordRequest.java
+++ src/main/java/com/tikitaka/backend/user/dto/UserChangePasswordRequest.java
@@ -4,15 +4,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Schema(description = "비밀번호 변경 요청")
 public record UserChangePasswordRequest(
-    @Schema(description = "현재 비밀번호", example = "1234")
+    @Schema(description = "현재 비밀번호", example = "12345678")
     @JsonProperty("current_password")
     @NotBlank(message = "현재 비밀번호는 필수입니다")
     String currentPassword,
 
-    @Schema(description = "새 비밀번호", example = "5678")
+    @Size(min = 8, message = "새 비밀번호는 최소 8자 이상이어야 합니다")
+    @Schema(description = "새 비밀번호", example = "876543421")
     @JsonProperty("new_password")
     @NotBlank(message = "새 비밀번호는 필수입니다")
     String newPassword
